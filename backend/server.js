a296b2fb45671269dd50894a319ca2cf39b4906c
Sebastian Dominiak <sebastiandominiak92@gmail.com>
2023-08-13 21:18:03 +0200

    add localhost to cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000' // Zakładając, że Twój klient działa na porcie 3000
+  }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
