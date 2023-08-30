caa60df2d51e45ee808b0670287aeb0809a03403
Sebastian Dominiak <sebastiandominiak92@gmail.com>
2023-08-30 16:47:50 +0200

    netlify fix

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,8 @@ connectDB();
 
 const app = express();
 app.use(cors({
-    origin:'http://localhost:3000' // Zakładając, że Twój klient działa na porcie 3000
+    origin: 'https://dazzling-bienenstitch-1eab4c.netlify.app' 
+    // || 'http://localhost:3000' // Zakładając, że Twój klient działa na porcie 3000
   }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
